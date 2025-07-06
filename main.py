ba2efdc26c0e416c747f439a5ed8de466c88e16f
Anmol Pujari <anmol.a1b2@gmail.com>
2025-07-06 19:02:46 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,35 @@
+import requests
+import discord
+
+CONTENT_TYPES = ["image/png", "image/jpeg", "video/mp4"]
+DEFAULT_USER_AGENT = "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:47.0) Gecko/20100101 Firefox/47.0"
+
+def download_file(filename, url):
+   response = requests.get(url, params={"User-Agent": DEFAULT_USER_AGENT})
+   with open(filename, mode="wb") as file:
+        file.write(response.content)
+
+def get_filetype(filename):
+    return filename.split('.')[-1].split(':')[0]
+
+def get_save_path(message, filename):
+    return f"C:\\Art\\{message.created_at.strftime("%Y-%m-%d")}_{message.author.name}.{get_filetype(filename)}"
+
+class Mereno(discord.Client):
+    async def on_ready(self):
+        print("All set, Sensei!")
+
+    async def on_message(self, message):
+        for attachment in message.attachments:
+           if attachment.content_type in CONTENT_TYPES:
+               download_file(get_save_path(message, attachment.filename), attachment.url)
+
+        for embed in message.embeds:
+            if embed.image:
+                download_file(get_save_path(message, embed.image.url), embed.image.url)
+
+with open(r"C:\Development\Mereno\token.txt") as file:
+    token = file.read()
+
+client = Mereno(intents=discord.Intents.all())
+client.run(token)
